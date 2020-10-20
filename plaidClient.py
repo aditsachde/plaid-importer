b9571ed22fdefccb962a569faa97b4cceeb4db9c
Adit Sachde <23707194+aditsachde@users.noreply.github.com>
2020-10-20 03:02:17 -0400

    fix config file database url

--- plaidClient.py
+++ plaidClient.py
@@ -5,9 +5,9 @@ from dotmap import DotMap
 with open("credentials.yaml") as file:
     c = DotMap(yaml.full_load(file))
     creds = DotMap(access_token=c["accessToken"],
-                   item_id=c["itemId"], databse=c["database"])
+                   item_id=c["itemId"])
     plaid_creds = DotMap(client_id=c["clientId"], secret=c["secret"],
-                         environment=c["environment"], webhook=c["webhook"])
+                         environment=c["environment"], webhook=c["webhook"], database=c["database"])
     # Access using creds.access_token and creds.item_id
 
 plaidClient = Client(plaid_creds.client_id,
